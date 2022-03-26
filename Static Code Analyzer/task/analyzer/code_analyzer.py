aa16a4d41f6b5eb62097d590ae5e2f212c5abcb1
Eyal_Bahar <eyal.bhr@gmail.com>
2022-03-26 19:59:28 +0700

    stage 4 complete

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -2,15 +2,23 @@ import re
 import sys
 import os
 
+
+def print_msg(line_number, msg):
+    global filename
+    print(f"{filename}: Line {line_number}: {msg}")
+
+
 def check_too_long(line, line_number):
     if len(line) > 79:
-            print(f"Line {line_number}: S001 Too long")
+        msg = "S001 Too long"
+        print_msg(line_number, msg) # print(f"Line {line_number}: S001 Too long")
 
 def check_indentation(line, line_number):
     import re
     indentation_length = len(re.findall("^ *", line)[0])
     if (indentation_length % 4) != 0:
-        print(f"Line {line_number}: S002 Indentation is not a multiple of four")
+        msg = "S002 Indentation is not a multiple of four"
+        print_msg(line_number, msg)
 
 def numer_of_quotes_after(line, idx):
     pattern1 = "[\'\"]"
@@ -36,7 +44,8 @@ def check_semi_col(line, line_number):
         in_a_string = (n_qoutes % 2)
         in_a_comment = check_if_in_a_comment(line, semi_col_idx)  # if there is "#" before ";"
         if not in_a_string and not in_a_comment: # if its even its not part of a string
-            print(f"Line {line_number}: S003 Unnecessary semicolon")
+            msg = "S003 Unnecessary semicolon"
+            print_msg(line_number, msg)
             return
 
 
@@ -51,7 +60,8 @@ def check_hash_space(line, line_number):
         in_a_comment = check_if_in_a_comment(line, hash_idx)
         if not in_a_string and not in_a_comment:
             if line[hash_idx - 1] != " " or line[hash_idx - 2] != line[hash_idx - 1]:
-                print(f"Line {line_number}: S004 At least two spaces required before inline comments")
+                msg = "S004 At least two spaces required before inline comments"
+                print_msg(line_number, msg)
                 return is_comment_line
     return is_comment_line
 
@@ -62,7 +72,8 @@ def check_TODO(line, line_number):
     for TODO_idx in TODO_idxs:
         in_a_comment = check_if_in_a_comment(line, TODO_idx)
         if in_a_comment:
-            print(f"Line {line_number}: S005 TODO found")
+            msg = "S005 TODO found"
+            print_msg(line_number, msg)
             return
 
 
@@ -72,8 +83,33 @@ def check_blank_lines(line, line_number, blank_lines):
     if is_blank_line:
         blank_lines.append(line_number)
     if (line_number-3) in blank_lines and (line_number-1) in blank_lines and (line_number-2) in blank_lines:
-        print(f"Line {line_number}: S006 More than two blank lines used before this line")
+        msg = "S006 More than two blank lines used before this line"
+        print_msg(line_number, msg)
 
+
+def class_construction_line(line):
+    first_char_idx = len(line) - len(line.lstrip())
+    if line[first_char_idx: (first_char_idx +5)] == "class":
+        return True
+
+
+def func_construction_line(line):
+    first_char_idx = len(line) - len(line.lstrip())
+    if line[first_char_idx: (first_char_idx + 3)] == "def":
+        return True
+
+
+def check_spaces_at_declaration(line, line_number):
+    if class_construction_line(line):
+        striped_line = line.lstrip(" ").lstrip("class")
+        if striped_line[0] == " " and striped_line[1] == " ":
+            msg = "S007 Too many spaces after 'class'"
+            print_msg(line_number, msg)
+    if func_construction_line(line):
+        striped_line = line.lstrip(" ").lstrip("def")
+        if striped_line[0] == " " and striped_line[1] == " ":
+            msg = "S007 Too many spaces after 'def'"
+            print_msg(line_number, msg)
 
 def list_files_from_input(input_path):
     """ Return a list of files to check on """
@@ -86,11 +122,32 @@ def list_files_from_input(input_path):
     all_files = os.listdir(input_path)
     for file in all_files:
         if file.endswith(".py"):
-            file_list.append(file)
+            file_list.append("\\".join([input_path, file]))
     return file_list
+
+
+def check_class_camel_case(line, line_number):
+    if class_construction_line(line):
+        class_name = line.lstrip(" ").lstrip("class").lstrip(" ").split('(')[0]
+        class_name = class_name.split(':')[0]
+        if class_name[0] != class_name[0].upper():
+            msg = f"S008 Class name '{class_name}' should use CamelCase"
+            print_msg(line_number, msg)
+
+
+def check_func_snake_case(line, line_number):
+    if func_construction_line(line):
+        func_name = line.lstrip(" ").lstrip("def").lstrip(" ").split('(')[0]
+        if not bool(re.match(r"^([^A-Z]+)+$", func_name)):
+        # if ((func_name[0] == func_name[0].upper()) and (func_name[0] != func_name[0].lower())):
+            msg = f"S009 Function name '{func_name}' should use snake_case"
+            print_msg(line_number, msg)
+
 
 def static_code_analyzer(file):
     with open(file) as f:
+        global filename
+        filename = file
         Lines = f.readlines()
         line_number = 0
         blank_lines = []
@@ -103,18 +160,20 @@ def static_code_analyzer(file):
             # if not is_comment_line:
             check_TODO(line, line_number)
             check_blank_lines(line, line_number, blank_lines)
-
+            check_spaces_at_declaration(line, line_number)
+            check_class_camel_case(line, line_number)
+            check_func_snake_case(line, line_number)
 
 
 def main():
     # test = r"C:\hyper_skill\static_code_analyzer_project\Static Code Analyzer\task\analyzer\test.txt"
     # code_path = input()
     input_path = r"C:\hyper_skill\static_code_analyzer_project\Static Code Analyzer\task\test\test_6.py"
-    import sys
      # can be directoy or file
     file_list = list_files_from_input(sys.argv[1])
     for file in file_list:
         static_code_analyzer(file)
+
 
 if __name__ == '__main__':
     main()
